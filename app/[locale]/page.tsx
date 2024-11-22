9e7389f18b2b36ba73afad3cd12f93c7d1a27448
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2024-11-22 17:31:16 +0700

    22-11-2567-v-3

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 // app/[locale]/page.tsx
-// ต้องการให้ไปที่ /locale/home
+
 import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/routing';
 import { redirect } from 'next/navigation';
@@ -8,5 +8,7 @@ import { redirect } from 'next/navigation';
 export default function HomePage({params} : {params : {locale : string}}) {
   const t = useTranslations('');
   redirect(`/${params.locale}/home`);
+
+  return null;
 
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// http://localhost:5173/th 
 // app/page.tsx
-// โชว์หน้านี้ ไม่ใช่ /app/[locale]/page.tsx
+
+
+
+import PageFromLocale from './[locale]/page'
 export default function HomePage() {
     return (
-      <main>
-        <h1>Welcome to the Home Page</h1>
-      </main>
+      <PageFromLocale params={{ locale: 'th' }} />
     );
   }
